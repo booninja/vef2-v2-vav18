2856ec2640dc5c9452001b48c5a211a1a1d1c535
booninja <vav18@hi.is>
2021-02-12 13:49:11 +0000

    Fix SSL heroku villu allegdedly

--- src/db.js
+++ src/db.js
@@ -5,12 +5,15 @@ dotenv.config();
 
 const {
   DATABASE_URL: connectionString,
+  NODE_ENV: nodeEnv = 'development'
 } = process.env;
 
 if (!connectionString) {
   console.error('Vantar DATABASE_URL');
   process.exit(1);
 }
+
+const ssl = nodeEnv !== 'development' ? { rejectUnauthorized: false } : false;
 
 const pool = new pg.Pool({ connectionString });
 
