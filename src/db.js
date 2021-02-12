f884646cc0b4c29b8bfe55555bb3cc29a3b11337
booninja <vav18@hi.is>
2021-02-12 13:53:53 +0000

    Fix SSL heroku villu allegdedly

--- src/db.js
+++ src/db.js
@@ -15,7 +15,7 @@ if (!connectionString) {
 
 const ssl = nodeEnv !== 'development' ? { rejectUnauthorized: false } : false;
 
-const pool = new pg.Pool({ connectionString });
+const pool = new pg.Pool({ connectionString, ssl });
 
 pool.on('error', (err) => {
   console.error('Unexpected error on idle client', err);
